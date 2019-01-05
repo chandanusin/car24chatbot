c8af218aa1924987ae56e04607d0be3989699195
Kumar, Adarsh <ADARSH.KUMAR@dish.com>
2019-01-05 20:49:03 +0530

    adding buyer Details

---
+++ Chat Bot/BuyerDetails.swift
@@ -0,0 +1,26 @@
+//
+//  BuyerDetails.swift
+//  Chat Bot
+//
+//  Created by Adarsh on 05/01/19.
+//  Copyright © 2019 Pallav Trivedi. All rights reserved.
+//
+
+import Foundation
+
+class BuyerDetails: NSObject {
+
+    var name:String?
+    var location:String?
+    var vehicleModel:String?
+    var name:String?
+    var name:String?
+    var name:String?
+
+    
+    
+    
+
+}
+
+
